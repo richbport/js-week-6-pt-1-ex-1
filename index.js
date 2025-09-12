94853fcc099700f448ed541a91a4ebc12f820c3a
Rich <richbport@yahoo.com>
2025-09-12 11:13:57 -0400

    setTimeout

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ function throttle(func, limit) {
     return function () {
         const args = arguments;
         const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => (inThrottle = false), limit);
+        }
     }
 }
 
