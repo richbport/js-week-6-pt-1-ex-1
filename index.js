2903f3f2d2502b34e36cb18eb6b98f43fc02260d
Rich <richbport@yahoo.com>
2025-09-12 10:50:49 -0400

    function throttle

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// function throttle(func, limit) {
+//     let inThrottle;
+//     return function () {
+//         const args = arguments;
+//         const context = this;
+//         if (!inThrottle) {
+//             func.apply(context, args);
+//             inThrottle = true;
+//             setTimeout(() => (inThrottle = false), limit);
+//         }
+//     };
+// }
+
 function throttle(func, limit) {
     let inThrottle;
-    return function () {
-        const args = arguments;
-        const context = this;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit);
-        }
-    };
 }
 
 let scrollCount = 0;
